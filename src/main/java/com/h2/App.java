f8aa697f97e7f411c17b39ef997fc51cb2ce5147
Nathan Gizaw <ngizaw@purdue.edu>
2021-08-11 22:06:09 -0700

    Module2 done

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -1,17 +1,22 @@
 package com.h2;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String [] args){
+		System.out.println("Hello World!" );
+	}
+	
+	public static int doubleTheNumber(int number){
+		return number * 2;
+	}
 
-    public static int doubleTheNumber(int number) {
-        return -1;
-    }
+	private static int add(int[] numbers){
+		int sum = 0;
+
+		for (int num: numbers){
+			sum += num;
+		}
+		return sum;
+	}	
+
 }
+
